2a944e872590a241dbd57ebeb30e463c59ffb86c
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2023-12-08 13:26:40 -0600

    create a static method in JavaScript and learn about its differences with other programming languages where truly static methods don't have access to the class

--- Object-Oriented-JavaScript/Static-Properties/index.js
+++ Object-Oriented-JavaScript/Static-Properties/index.js
@@ -9,6 +9,18 @@ class Cat {
     this.breed = breed;
   }
 
+  /**
+   *  Almost every other OO language more properly calls this a "class method"
+   * not a static method - since it does have access to this class itself
+   * (that's what the "this" is in a jS "static method")
+   *
+   * More consistent OO languages, like C++/Java/Python, also have true static
+   * methods, where they don't have access to the class itself.
+   */
+  static method() {
+    console.log('this is:', this);
+  }
+
   describe() {
     return `${this.name} is a ${Cat.genusSpecies}`;
   }
